a1a49bb9fd27eb80e9f4861e1a5beacad50f87f5
Arthur Queiroz <94507280+arthuurqueirozz@users.noreply.github.com>
2023-08-05 18:26:23 -0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,61 @@
+let board = ['','','','','','','','','']
+let playerTime = 0
+let gameOver = false;
+let count = 0
+
+let symbols=['o', 'x']
+let winStates = [
+    [0, 1, 2],
+    [3, 4, 5],  
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]  
+]
+
+function handleMove(position){    
+    
+    if(gameOver) 
+        return 
+
+    if (board[position] == ''){
+        
+        board[position] = symbols[playerTime]
+        
+        gameOver = win()
+
+        if(!gameOver)
+
+        
+            if(playerTime == 0){
+                playerTime = 1
+                count ++
+            }else{
+                playerTime = 0 
+                count ++
+            } 
+    }
+
+    return gameOver
+}
+
+function win(){
+
+    for (let i = 0; i < winStates.length; i++){
+        let seq = winStates[i]
+        
+        let pos0 = seq[0]
+        let pos1 = seq[1]
+        let pos2 = seq[2]
+
+        if(board[pos0] == board[pos1] && 
+            board[pos0] == board[pos2] && board[pos0] != ''){
+            
+            return true 
+        }
+    }
+
+    return false
+}
---
+++ interface.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    let squares = document.querySelectorAll('.square')
+
+    squares.forEach((square) => {
+        square.addEventListener('click', handleClick)
+    })
+})
+
+function handleClick(event) {
+    let position = event.target.id
+
+    if(count == 8){
+        document.getElementById('winner').innerHTML = '<p>DRAW!</p>'
+    }
+
+    if(handleMove(position)){
+        
+        if (playerTime == 1) {
+            document.getElementById('winner').innerHTML = '<p>The winner was X</p>'
+        } else{
+            document.getElementById('winner').innerHTML = '<p>The winner was O</p>'
+
+        }
+        
+    }
+    updateSquare(position)
+    if(count == 8)
+        return gameOver == true 
+
+  
+}
+
+function updateSquare(position){
+    let square = document.getElementById(position.toString())
+    let symbol = board[position]
+    square.innerHTML = `<div class="${symbol}"></div>`
+}
+
+function restartGame(){
+    board = ['','','','','','','','','']
+    playerTime = 0
+    gameOver = false
+    count = 0
+
+    let squares = document.querySelectorAll('.square')
+    
+    squares.forEach((square) => {
+        let symbol = board[square.id]
+        square.innerHTML = `<div class="${symbol}"></div>`  
+        document.getElementById('winner').innerText = ''
+    })
+}
